1a51d15dbcb01e9b75d55f72220aa93d6a4f6227
dangbaoquoc <quocdb3@fpt.com>
2024-05-08 11:36:45 +0700

    'first'

---
+++ src/main/java/com/clothes/datn/DatnApplication.java
@@ -0,0 +1,22 @@
+package com.clothes.datn;
+
+import com.clothes.datn.service.FilesStorageService;
+import jakarta.annotation.Resource;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnApplication {
+//    @Resource
+//    FilesStorageService storageService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnApplication.class, args);
+    }
+
+//    @Override
+//    public void run(String... arg) throws Exception {
+//        storageService.init();
+//    }
+}
